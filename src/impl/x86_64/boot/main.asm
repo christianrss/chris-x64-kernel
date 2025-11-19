cdd2d47159985fbc461bdb95c03776003921c21f
Christian Rafael <christianparadix@gmail.com>
2025-11-19 00:39:39 -0300

    Enter long mode

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -1,8 +1,10 @@
 ;; Constants
 STACKSIZE       equ 4096 * 4
 MULTIBOOTMAGIC  equ 0x36d76289
-CHECKLONGMODE		equ 0x80000000
+CHECKLONGMODE	equ 0x80000000
 CHECKLONGMODE2 	equ 0x80000001
+PAGETABLESIZE   equ 4096
+ENABLELONGMODE  equ 0xC0000080
 
 global start
 
@@ -14,6 +16,9 @@ start:
     call check_multiboot
     call check_cpuid
     call check_long_mode
+
+    call setup_page_tables
+    call enable_paging
 
 	;; print `OK`
 	mov dword [0xb8000], 0x2f4b2f4f
@@ -61,14 +66,67 @@ check_long_mode:
 	mov al, "L"
 	jmp error
 
+setup_page_tables:
+    mov eax, page_table_l3
+    or eax, 0b11 ; present, writable
+    mov [page_table_l4], eax
+
+    mov eax, page_table_l2
+    or eax, 0b11 ; present, writable
+    mov [page_table_l3], eax
+
+    mov ecx, 0 ; counter
+
+.loop:
+    mov eax, 0x200000   ; 2MiB
+    mul ecx
+    or eax, 0b10000011  ; present, writable, huge page
+    mov [page_table_l2 + ecx * 8], eax
+
+    inc ecx      ; increment counter
+    cmp ecx, 512 ; checks if the whole table is mapped
+    jne .oop     ; if not, continue
+
+    ret
+
+enable_paging:
+    ; pass page table location to cpu
+    mov eax, page_table_l4
+    mov cr3, eax
+
+    ; Enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; Enable long mode
+    mov ecx, ENABLELONGMODE
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; Enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
+
 error:
 	mov dword [0xb8000], 0x4f524f45 ; Print "ERR: X" where X is the error code
 	mov dword [0xb8004], 0x4f3a4f52
 	mov dword [0xb8008], 0x4f204f20
-	mov byte	[0xb800a], al
+	mov byte  [0xb800a], al
 	hlt
 
 section .bss
+align PAGETABLESIZE
+page_table_l4:
+    resb PAGETABLESIZE
+page_table_l3:
+    resb PAGETABLESIZE
+page_table_l2:
+    resb PAGETABLESIZE
 stack_bottom:
     resb STACKSIZE
 stack_top:
